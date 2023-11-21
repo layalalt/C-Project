3eab3c060c3802a33990f59876a6c64c9e2261b9
sarahaak <151647302+sarahaak@users.noreply.github.com>
2023-11-21 17:50:42 +0300

    Update TaxiProject.c

--- TaxiProject.c
+++ TaxiProject.c
@@ -2,9 +2,29 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main()
-{
+struct Taxi {
+    int id;
+    char driver[20];
+    char category[20];
+    char plate[7];
+    char color[20];
+    float rate;
+    float minCharge;
+    char state;
+    struct Taxi* next;
+};
 
+int main() {
+    
+    FILE* file = fopen("Taxies.txt", "r"); 
 
-return 0; 
+    if (file == NULL) {
+        printf("file opening error");
+    }
+   else{
+   printf("file opened successfully");
+   //rest of the code
+   }
+    
+    return 0;
 }
